6a4a1b8b39d119455d52dd50252aa166391284d4
Andrew Timkov <antimkov@amazon.com>
2025-12-06 20:11:36 +1100

    Add day 06

---
+++ 06.py
@@ -0,0 +1,31 @@
+import math
+from itertools import groupby
+
+with open('06.txt', 'r') as f:
+	data_orig = [l.rstrip('\n') for l in f.readlines()]
+
+cw_rot = lambda matrix: list(map(list, zip(*reversed(matrix))))
+ccw_rot = lambda matrix: list(map(list, zip(*matrix)))[::-1]
+ops = {'*': math.prod, '+': sum}
+
+data = [l.split() for l in data_orig]
+
+part_1 = 0
+for c in range(len(data[0])):
+	operator = data[-1][c]
+	nums = [int(row[c]) for row in data[:-1]]
+	part_1 += ops[operator](nums)
+
+# for part 2: rotate CCW, group by problems, then solve each row (was column before rotate)
+rotated_data = ccw_rot(data_orig)
+grouped_data = groupby(rotated_data, key=lambda group: all(not x.strip() for x in group))
+problems = [list(g) for k, g in grouped_data if not k]
+
+part_2 = 0
+for problem in problems:
+	operator = problem[-1][-1]
+	nums = [int(''.join(x[:-1]).strip()) for x in problem]
+	part_2 += ops[operator](nums)
+
+print("Part 1:", part_1)
+print("Part 2:", part_2)

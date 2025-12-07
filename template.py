9eb6ad7f42e915f9cd084ef9ba49874d3b26d82c
Andrew Timkov <antimkov@amazon.com>
2025-12-07 19:01:34 +1100

    Add template

---
+++ template.py
@@ -0,0 +1,12 @@
+import math
+from functools import lru_cache
+from itertools import groupby
+
+with open('xx.txt', 'r') as f:
+	data = [l.rstrip() for l in f.readlines()]
+
+part_1 = 0
+part_2 = 0
+
+print("Part 1:", part_1)
+print("Part 2:", part_2)
